6922481aeb98d2f56820f132a86f9c36aa2487a3
Byron Foster <byron@base2.cc>
2011-04-22 14:41:22 -0700

    Fix compile errors from directory movement

--- free42/common/core_tables.h
+++ free42/common/core_tables.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * Free42 -- an HP-42S calculator simulator
- * Copyright (C) 2004-2011  Thomas Okken
+ * Copyright (C) 2004-2010  Thomas Okken
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2,
@@ -407,7 +407,10 @@
 #define CMD_RCLALM      365
 #define CMD_SWPT        366
 
-#define CMD_SENTINEL    367
+#define CMD_UNDO        367
+#define CMD_REDO        368
+
+#define CMD_SENTINEL    369
 
 
 /* command_spec.argtype */
